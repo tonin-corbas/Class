82bff16bcbc1c065e04dc983487834c9cd662c8d
Toñin <j.antoniokumar@gmail.com>
2024-03-05 20:28:12 +0100

    PAso a paso

--- 2a Evaluación/tarea_13/src/components/Navegador.tsx
+++ 2a Evaluación/tarea_13/src/components/Navegador.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Offcanvas from 'react-bootstrap/Offcanvas';
-
+import logo from "../img/logo.png";
+import fondo2 from "../img/fondo2.png";
 const Navegador = () => {
   return (
-    <Navbar expand="lg" className="bg-danger">
+    <Navbar expand="lg" className="bg-image text-dark"  bg="dark" data-bs-theme="dark" style={{ backgroundImage: `url(${fondo2})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+      <Navbar.Brand as={Link} to='/'>
+        <img src={logo} width='70' alt='Logo'></img>
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="offcanvasNavbar"/>
       <Navbar.Offcanvas
         id="offcanvasNavbar"
@@ -15,13 +20,13 @@ const Navegador = () => {
         <Offcanvas.Header closeButton className='bg-danger text-center'>
           <Offcanvas.Title id="offcanvasNavbarLabel" className='text-light'>Menú</Offcanvas.Title>
         </Offcanvas.Header>
-        <Offcanvas.Body className='bg-danger flex-row'>
+        <Offcanvas.Body className=' bgd-flex justify-content-center' >
           <Nav>
-            <Nav.Link className="px-3 text-light" href="/">Inicio</Nav.Link>
-            <Nav.Link className="px-3 text-light" href="/points-of-interest">Puntos de Interés</Nav.Link>
-            <Nav.Link className="px-3 text-light" href="/restaurants">Restaurantes</Nav.Link>
-            <Nav.Link className="px-3 text-light" href="/hotels">Hoteles</Nav.Link>
-            <Nav.Link className="px-3 text-light" href="/leisure-activities">Actividades de Ocio</Nav.Link>
+            <Nav.Link className="px-3 text-dark fw-bold" href="/">Inicio</Nav.Link>
+            <Nav.Link className="px-3 text-dark fw-bold" href="/points-of-interest">Puntos de Interés</Nav.Link>
+            <Nav.Link className="px-3 text-dark fw-bold" href="/restaurants">Restaurantes</Nav.Link>
+            <Nav.Link className="px-3 text-dark fw-bold" href="/hotels">Hoteles</Nav.Link>
+            <Nav.Link className="px-3 text-dark fw-bold" href="/leisure-activities">Actividades de Ocio</Nav.Link>
           </Nav>
         </Offcanvas.Body>
       </Navbar.Offcanvas>
--- 2a Evaluación/tarea_13/src/views/Hotel.tsx
+++ 2a Evaluación/tarea_13/src/views/Hotel.tsx
@@ -1,13 +1,36 @@
 import React from 'react';
+import { Container, Row, Col, Image } from 'react-bootstrap';
+import camp from "../img/camp.png";
 
 const Hotel = () => {
+  // Array de datos simulados (puedes reemplazarlo con tus propios datos)
+  const hotelData = [
+    { camp, text: 'En este lugar podras hospedarle con unos increíbles lujos en el caso de que seas un fiel seguidor de la Absoluta. En caso de que seas un elegidode la misma serás considerado como un apóstol, en cambio si eres alguien que aborrezca a la Absoluta el precio de hospedarte será tu vida. Buena suerte' },
+    { imageUrl: 'ruta_de_la_imagen2.jpg', text: 'Texto para la parte 2' },
+    { imageUrl: 'ruta_de_la_imagen3.jpg', text: 'Texto para la parte 3' },
+    // ... Repite para las otras 9 partes ...
+  ];
+
   return (
-    <div className='text-center'>
-      <h1>Hoteles</h1>
-      <p>Aquí se mostrarán las opciones de alojamiento, con detalles como el precio, ubicación y servicios ofrecidos.</p>
-    </div>
+    <Container>
+      <Row>
+        {hotelData.slice(0, 6).map((part, index) => (
+          <Col key={index} xs={12} md={4}>
+            <Image src={part.camp} fluid />
+            <p>{part.text}</p>
+          </Col>
+        ))}
+      </Row>
+      <Row>
+        {hotelData.slice(6).map((part, index) => (
+          <Col key={index} xs={12} md={4}>
+            <Image src={part.imageUrl} fluid />
+            <p>{part.text}</p>
+          </Col>
+        ))}
+      </Row>
+    </Container>
   );
 };
 
 export default Hotel;
-

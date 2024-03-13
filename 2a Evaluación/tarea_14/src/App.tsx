5818be1ae4b68676fae7b64a7c8ae844274965cf
Toñin <j.antoniokumar@gmail.com>
2024-03-13 15:18:05 +0100

    Arreglado

--- 2a Evaluación/tarea_14/src/App.tsx
+++ 2a Evaluación/tarea_14/src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Breeds from './Views/Razas';
+import Breeds from './Views/Breeds';
+import { BrowserRouter } from 'react-router-dom';
 
 
 function App() {
   return (
-    <Breeds/>
+    <BrowserRouter>
+      <Breeds />
+    </BrowserRouter>
   );
 }
 

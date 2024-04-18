907e867d26097f48173d5b534071b378569b6987
Toñin <j.antoniokumar@gmail.com>
2024-04-18 16:33:20 +0200

    a

--- 2a Evaluación/tarea_15/src/App.tsx
+++ 2a Evaluación/tarea_15/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Breeds from './Views/Breeds';
+import Breeds from './Views/Principal';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Navegador from './Componentes/Navegador';
 import Inicio from './Views/Inicio';
---
+++ 2a Evaluación/tarea_15/src/Views/Principal.tsx
@@ -0,0 +1,9 @@
+export type Root = Root2[]
+
+export interface Root2 {
+  nombre: string
+  id: number
+  disponible: boolean
+  urlImagen: string
+  fecha?: string
+}

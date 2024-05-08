7010970307a1c85cb7ad2d575396af09648fabf5
Toñin <j.antoniokumar@gmail.com>
2024-05-08 18:06:18 +0200

    Pasos

--- 2a Evaluación/tarea_17/app.js
+++ 2a Evaluación/tarea_17/app.js
@@ -7,28 +7,45 @@ app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// home
 app.get("/", (req, res) => {
   const rows = db.prepare('SELECT * from Usuarios').all();
-  res.render("index", msgs = { msgs: rows })
+  res.render("app", msgs = { msgs: rows })
 })
 
+// lista de usuarios
+// no requiere parametros
 app.get('/usuarios', (req, res) => {
   const rows = db.prepare('SELECT * from Usuarios').all();
   res.json(rows)
 })
 
+// ver un usuario en concreto
+// debería usar query params
 app.get('/usuario', (req, res) => {
-  res.render("usuario");
+  const usuarioId = req.query.id;
+  const row = db.prepare('SELECT * FROM Usuarios WHERE id = ?').get(usuarioId)
+  res.json(row);
 })
 
-app.post("/usuario", (req, res) => {
+// devuelve el formulario
+// no requerie parametros
+app.get("/addusuario", (req, res) => {
+  const rows = db.prepare('SELECT * from Usuarios').all();
+  res.json(rows) 
+})
+
+//  añadir un nuevo usuario
+// los parametros son parte del body (formulario)
+app.post("/addusuario", (req, res) => {
   if (req.body) {
     if (req.body.nombre && req.body.email) {
       const statement = db.prepare('INSERT INTO Usuarios (nombre, email) VALUES (?,?)')
-      const info = statement.run(req.body.nombre, req.body.email)
+      const info = statement.run();
+      console.log(info);
     }
   }
-  res.redirect("usuario");
+  res.redirect("usuarios");
 })
 
 app.get('/productos', (req, res) => {
@@ -53,5 +70,5 @@ app.post("/producto", (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`)
 })
